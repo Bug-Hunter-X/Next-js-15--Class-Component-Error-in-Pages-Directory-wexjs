51da566f276f40f045c1a07dfd1ffe4962bae3d4
BugHunter-X <askzenai@gmail.com>
2024-12-09 12:28:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,26 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error if you are using Next.js 13 or later.
+  // It needs to be a function component
+  return (
+    <h1>About Page</h1>
+  );
+}
+```
